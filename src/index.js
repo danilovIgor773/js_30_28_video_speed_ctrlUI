ce5f5aa069dd76a95a8207f0fae2013edc7264f7
Danilov Igor <danilovigor773@gmail.com>
2018-10-16 13:24:24 +0500

    Lesson 28 - js30. Video controller UI. Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './index.scss';
 
-console.log("Hello World");
+const speed = document.querySelector('.speed');
+const bar = document.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
+//console.log(video);
+
+function handleMove(e){
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const height = Math.round(percent * 100) + '%';
+  //console.log(height);
+  const playbackRate = percent * (max - min) + min;
+  console.log(playbackRate);
+  bar.style.height = height;
+  bar.textContent = playbackRate.toFixed(2) + 'x';
+  video.playbackRate = playbackRate;
+
+}
+
+speed.addEventListener('mousemove', handleMove);
